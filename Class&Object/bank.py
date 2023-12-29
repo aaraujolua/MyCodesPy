d6495961e587f90cfe739f9bc63c88ab5adc7ded
Luana Araújo <f.araujoluana@gmail.com>
2023-12-29 12:02:14 -0300

    UPDATE Class&Object

--- Class&Object/bank.py
+++ Class&Object/bank.py
@@ -4,25 +4,25 @@ class BankAccount:
         self.type = type
         self.agency = agency
         self.account = account
-        self.balance = balance
+        self.__balance = balance
         
     def showDataAccount(self):
         print(f'Account holder: {self.holder}')
-        print(f'Account type: {self.type}')
+        print(f'Account type: {self.type}') 
         print(f'Agency: {self.agency}')
         print(f'Account: {self.account}')
-        print(f'Balance: ${self.balance} \n')
+        print(f'Balance: ${self.__balance} \n')
         
     def withdraw(self, value):
-        if self.balance < value:
+        if self.__balance < value:
             print("Sorry, you don't have enough balance to make this operation.")
         else:
-            self.balance -= value
-            print(f'{self.holder} withdrawal made successfully! Your current balance is ${self.balance} \n')
+            self.__balance -= value
+            print(f'{self.holder} withdrawal made successfully! Your current balance is ${self.__balance} \n')
             
     def deposit(self, value):
         self.balance += value
-        print(f'{self.holder} deposit made successfully! Your current balance is ${self.balance} \n')
+        print(f'{self.holder} deposit made successfully! Your current balance is ${self.__balance} \n')
         
         
         
@@ -31,7 +31,6 @@ if __name__ == '__main__':
     luana_account = BankAccount("Luana", "Current", "0001", 102030, 300000)
     james_account = BankAccount("James", "Savings", "0001", 203040, 200000)
     
-
     print(luana_account.type)
     james_account.showDataAccount()
     luana_account.showDataAccount()
@@ -41,5 +40,6 @@ if __name__ == '__main__':
     
     value_deposit = float(input('Enter the value you want to deposit: '))
     luana_account.deposit(value_deposit)
+    
     
     
